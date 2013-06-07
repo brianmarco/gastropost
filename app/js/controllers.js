8697994d969af3c5235955cf4dd1d6c933485a19
Brian Marco <brianmarco@gmail.com>
2013-06-06 23:41:21 -0400

    Functioning wall

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,27 +5,46 @@
 angular.module('gastropostAngular.controllers', [])
 
 .controller('WallController', ['$scope', '$resource', '$http', function(scope, resource, http) {
-    scope.welcome = 'Gastropost';
+  scope.brand = 'Gastropost Wall';
 
-    scope.tweets = null;
+  scope.tweets = null;
 
-    scope.twitter = function() {
-        http({
-            method: 'GET',
-            url: '/api/twitter/tweets',
-        })
-        .success(function(data, status) {
-            console.log('SUCCESS');
-            console.dir(data);
-            scope.tweets = data;
-        })
-        .error(function(data, status) {
-            console.log('FAIL');
-            console.dir(data);
-        });
-    }
+  scope.instagrams={};
 
-    scope.twitter();
+  scope.twitter = function() {
+    http({
+      method: 'GET',
+      url: '/api/twitter/tweets',
+    })
+    .success(function(data, status) {
+      scope.tweets = data;
+
+      for (var i=0; i<scope.tweets.statuses.length; i++) {
+        var status = scope.tweets.statuses[i];
+
+        if (status.entities && status.entities.urls) {
+          var urls = status.entities.urls;
+
+          var INSTAGRAM_URL_PATTERN = /instagram\.com/;
+
+          for (var j=0; j<urls.length; j++) {
+            var url = urls[j];
+
+            if (url.expanded_url.match(INSTAGRAM_URL_PATTERN)) {
+              scope.instagrams[status.id] = url.expanded_url + 'media';
+              break;
+            }
+          }
+        }
+      }
+    })
+    .error(function(data, status) {
+      console.log('FAIL');
+      console.dir(data);
+    });
+  }
+
+  scope.twitter();
 }])
 
 .controller('DetailsController', ['$scope', function(scope) {
